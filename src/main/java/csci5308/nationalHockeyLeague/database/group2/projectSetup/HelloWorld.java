09f3b22c5abd74cd3c8934c4eb987f2670f7b35d
souvikdas <Souvik.Das@dal.ca>
2020-09-26 23:51:29 -0300

    Initial Commit with folder structure

---
+++ src/main/java/csci5308/nationalHockeyLeague/database/group2/projectSetup/HelloWorld.java
@@ -0,0 +1,11 @@
+package csci5308.nationalHockeyLeague.database.group2.projectSetup;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+
+    public static String message(){
+        return "Hello World";
+    }
+}
