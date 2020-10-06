2094975e92e3c2a10a2107880b597a3e016a38e2
Nikunj Goenka <nk53359@Dal.ca>
2020-10-06 19:45:11 +0530

    fixed faling test issue

--- src/test/java/dhl/simulationStateMachineTest/JsonFilePathMock.java
+++ src/test/java/dhl/simulationStateMachineTest/JsonFilePathMock.java
@@ -4,7 +4,7 @@ import dhl.simulationStateMachine.Interface.IJsonFilePath;
 
 public class JsonFilePathMock implements IJsonFilePath {
 
-    String filePath = "N://STUDIES//MACS//SEM-1//CSCI5308 - Adv Topics in Software Develop//Project//csci5308//src//test//java//dhl//simulationStateMachineTest//MockData.json";
+    String filePath = "src//test//java//dhl//simulationStateMachineTest//MockData.json";
     @Override
     public String getFilePath() {
         return filePath;
