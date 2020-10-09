c4bdc7ea80b908045171e0126daef24de3b5bbf5
Rashmi Chandy <rs682521@dal.ca>
2020-10-09 05:44:22 +0530

    Adding test to check same team name in a conference

--- src/main/java/dhl/leagueModel/Division.java
+++ src/main/java/dhl/leagueModel/Division.java
@@ -3,7 +3,11 @@ package dhl.leagueModel;
 import dhl.leagueModel.interfaceModel.IDivision;
 import dhl.leagueModel.interfaceModel.ITeam;
 import dhl.leagueModel.interfaceModel.IValidation;
+
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Division implements IDivision {
     private int divisionId;
@@ -42,6 +46,13 @@ public class Division implements IDivision {
 
     public boolean checkIfDivisionValid(IValidation validation) throws Exception{
         validation.isStringEmpty(divisionName,"Division name");
+        List<String> teamNames=new ArrayList<>();
+        teams.stream().map(team-> team.getTeamName()).forEach(name->teamNames.add(name));
+        Set<String> teamsSet = new HashSet<>(teamNames);
+        if (teamsSet.size() < teamNames.size()) {
+            throw new Exception("The names of teams inside a division must be unique");
+        }
+
         return true;
     }
 
