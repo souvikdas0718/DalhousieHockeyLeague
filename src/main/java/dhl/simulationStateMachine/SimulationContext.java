d3c598427a23344f6353daf40eca20b5368c76bd
Nikunj Goenka <nk53359@Dal.ca>
2020-10-19 03:28:41 +0530

    added gameConfig to nested state Machine

--- src/main/java/dhl/simulationStateMachine/SimulationContext.java
+++ src/main/java/dhl/simulationStateMachine/SimulationContext.java
@@ -1,21 +1,29 @@
 package dhl.simulationStateMachine;
 
+import dhl.importJson.Interface.IGameConfig;
 import dhl.simulationStateMachine.Interface.ISimulationSeasonState;
 import dhl.simulationStateMachine.States.SeasonSimulationState;
 
 public class SimulationContext {
 
     ISimulationSeasonState seasonSimulationState;
-
     ISimulationSeasonState currentSimulation;
+    IGameConfig gameConfig;
 
     public SimulationContext(){
         seasonSimulationState = new SeasonSimulationState(this);
-
         currentSimulation = seasonSimulationState;
     }
 
     public void startSeasonSimulation(int seasonNumber){
         currentSimulation.startSeasonSimulation(seasonNumber);
+    }
+
+    public IGameConfig getGameConfig() {
+        return gameConfig;
+    }
+
+    public void setGameConfig(IGameConfig gameConfig) {
+        this.gameConfig = gameConfig;
     }
 }
