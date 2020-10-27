1318e79a024ac34e41389edf5453d32e15aaab49
Nikunj Goenka <nk53359@Dal.ca>
2020-10-27 09:54:26 +0530

    s

--- src/main/java/dhl/trading/Trading.java
+++ src/main/java/dhl/trading/Trading.java
@@ -1,16 +1,30 @@
 package dhl.trading;
 
 import dhl.importJson.Interface.IGameConfig;
+import dhl.leagueModel.interfaceModel.ITeam;
 import dhl.simulationStateMachine.Interface.ISimulationSeasonState;
 import dhl.trading.Interface.ITradeConfig;
 
-public class ExecuteTrade {
+public class Trading {
 
     IGameConfig ourGameConfig;
     ITradeConfig tradeConfig;
 
-    public ExecuteTrade(IGameConfig gameConfig){
+
+    public Trading(IGameConfig gameConfig){
         ourGameConfig = gameConfig;
         tradeConfig = new TradeConfig(ourGameConfig);
     }
+
+    public void startTrading(){
+        // TODO: 26-10-2020 call method to calculat
+
+    }
+
+    public void findLossPointOfTheTeam(ITeam team){
+
+        // TODO: 27-10-2020 Talk to team about getting loss points
+    }
+
+
 }
