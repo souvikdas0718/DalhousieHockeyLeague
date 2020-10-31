fc8ef598da16b709d40e19522eeac9dd5ad40d33
souvikdas <Souvik.Das@dal.ca>
2020-10-31 02:20:48 -0300

    Adding Standing System Test class

--- src/main/java/dhl/simulationStateMachine/States/standings/StandingSystem.java
+++ src/main/java/dhl/simulationStateMachine/States/standings/StandingSystem.java
@@ -6,15 +6,12 @@ import dhl.leagueModel.interfaceModel.ITeam;
 import dhl.simulationStateMachine.Interface.IStandingSystem;
 import dhl.simulationStateMachine.Interface.IStandings;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class StandingSystem implements IStandingSystem {
     private List<IStandings> standingsList;
-
-    public StandingSystem(List<IStandings> standingsList) {
-        this.standingsList = standingsList;
-    }
 
     public List<IStandings> getStandingsList() {
         return standingsList;
@@ -27,7 +24,7 @@ public class StandingSystem implements IStandingSystem {
     public void updateWinningStandings(ITeam team) {
 
         for (IStandings standings : standingsList) {
-            if (standings.getTeamName().equals(team.getTeamName())) {
+            if (standings.getTeamName().getTeamName().equals(team.getTeamName())) {
                 standings.setPoints(standings.getPoints() + 2);
                 standings.setWins(standings.getWins() + 1);
                 standings.setGamesPlayed(standings.getGamesPlayed() + 1);
@@ -38,7 +35,7 @@ public class StandingSystem implements IStandingSystem {
     public void updateLosingStandings(ITeam team) {
 
         for (IStandings standings : standingsList) {
-            if (standings.getTeamName().equals(team.getTeamName())) {
+            if (standings.getTeamName().getTeamName().equals(team.getTeamName())) {
                 standings.setLoss(standings.getLoss() + 1);
                 standings.setGamesPlayed(standings.getGamesPlayed() + 1);
             }
@@ -46,49 +43,49 @@ public class StandingSystem implements IStandingSystem {
     }
 
     public List<IStandings> divisionRanking(IDivision division) {
-        List<IStandings> divisionStandingsList = null;
-        List<IStandings> updatedDivisionStandingsList = null;
+        List<IStandings> divisionStandingsList = new ArrayList<>();
         for (IStandings standings : standingsList) {
             if (standings.getTeamDivision().equals(division.getDivisionName())) {
                 divisionStandingsList.add(standings);
             }
         }
-        updatedDivisionStandingsList = rankGenerator(divisionStandingsList);
-        return updatedDivisionStandingsList;
-
+        rankGenerator(divisionStandingsList);
+        return divisionStandingsList;
     }
 
     public List<IStandings> conferenceRanking(IConference conference) {
-        List<IStandings> conferenceStandingsList = null;
-        List<IStandings> updatedConferenceStandingsList = null;
+        List<IStandings> conferenceStandingsList = new ArrayList<>();
         for (IStandings standings : standingsList) {
             if (standings.getTeamConference().equals(conference.getConferenceName())) {
                 conferenceStandingsList.add(standings);
             }
         }
-        updatedConferenceStandingsList = rankGenerator(conferenceStandingsList);
-        return updatedConferenceStandingsList;
+        rankGenerator(conferenceStandingsList);
+        return conferenceStandingsList;
     }
 
     public List<IStandings> leagueRanking() {
-        List<IStandings> leagueStandingsList = null;
-        leagueStandingsList = rankGenerator(standingsList);
-        return leagueStandingsList;
+        List<IStandings> standingsList = new ArrayList<>();
+        standingsList = this.standingsList;
+        rankGenerator(standingsList);
+        return standingsList;
     }
 
-    public List<IStandings> rankGenerator(List<IStandings> rankList) {
+
+    public void rankGenerator(List<IStandings> rankList) {
         Collections.sort(rankList, (IStandings objectOne, IStandings objectTwo) -> {
-            if (Integer.compare(objectOne.getPoints(), objectTwo.getPoints()) != 0) {
-                return Integer.compare(objectOne.getPoints(), objectTwo.getPoints());
-            } else {
-                if (Integer.compare(objectOne.getWins(), objectTwo.getWins()) != 0) {
-                    return Integer.compare(objectOne.getWins(), objectTwo.getWins());
-                } else {
-                        return Integer.compare(objectOne.getLoss(), objectTwo.getLoss());
+            if (Integer.compare(objectOne.getPoints(), objectTwo.getPoints()) == 0){
+                if (Integer.compare(objectOne.getWins(), objectTwo.getWins()) == 0) {
+                    return Integer.compare(objectTwo.getLoss(), objectOne.getLoss());
+                }
+                else {
+                    return Integer.compare(objectTwo.getWins(), objectOne.getWins());
                 }
             }
+            else {
+                return Integer.compare(objectTwo.getPoints(), objectOne.getPoints());
+            }
         });
-        Collections.sort(rankList, Collections.reverseOrder());
-        return rankList;
     }
+
 }
