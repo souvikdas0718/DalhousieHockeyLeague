99d36f50b1911a22f3d537e16660d4fbebe2d6fc
puni <rj364202@dal.ca>
2020-11-02 04:56:23 +0530

    Database refactoring

--- src/main/java/dhl/database/interfaceDB/IPlayerDB.java
+++ src/main/java/dhl/database/interfaceDB/IPlayerDB.java
@@ -1,5 +1,6 @@
 package dhl.database.interfaceDB;
 
+import dhl.database.DatabaseObjectCreationDB;
 import dhl.leagueModel.interfaceModel.IPlayer;
 import dhl.leagueModel.interfaceModel.ILeagueObjectModel;
 import dhl.leagueModel.interfaceModel.IPlayer;
@@ -8,10 +9,15 @@ import java.util.List;
 import java.util.Map;
 
 public interface IPlayerDB {
+
     public int insertPlayer(IPlayer player, int teamId, int leagueId )  throws Exception;
+
+    public void updatePlayer(IPlayer player, String teamName, String leagueName )  throws Exception;
 
     public void insertRetiredPlayers(ILeagueObjectModel leagueObjectModel,
                                      Map<String, List<IPlayer>> playersToRetire,
                                      List<IPlayer> freeAgentsToRetire)  throws Exception;
+
+    public List<IPlayer> getPlayerList(int teamId,int leagueId) throws Exception;
 
 }
