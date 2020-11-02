cf9cf98b9c028febfaeb5530d0c5bb7b4e45880b
Rashmi Chandy <rs682521@dal.ca>
2020-11-02 23:47:07 +0530

    delete file

--- src/main/java/dhl/database/gameConfigDB.java
+++ src/main/java/dhl/database/gameConfigDB.java
@@ -1,24 +1,56 @@
 package dhl.database;
 
+import dhl.InputOutput.importJson.GameConfig;
 import dhl.InputOutput.importJson.Interface.IGameConfig;
 import dhl.database.DatabaseConfigSetup.CallStoredProcedure;
 import dhl.database.interfaceDB.IGameConfigDB;
+import org.json.simple.JSONObject;
+
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 
-public class gameConfigDB implements IGameConfigDB {
+public class GameConfigDB implements IGameConfigDB {
+    String leagueName;
     public void insertGamePlayConfig(IGameConfig gameConfig,String leagueName)  throws Exception {
+        this.leagueName=leagueName;
+
+        HashMap<String,String[]> config=new HashMap<>();
+        String[] agingValues={gameConfig.getAverageRetirementAge(),gameConfig.getMaximumAge()};
+        config.put(gameConfig.getAging(),agingValues);
+
+        String[] injuryValues={gameConfig.getRandomInjuryChance(),gameConfig.getInjuryDaysLow(),gameConfig.getInjuryDaysHigh()};
+        config.put(gameConfig.getInjuries(),injuryValues);
+
+        String[] gameResolverValues={gameConfig.getRandomWinChance()};
+        config.put(gameConfig.getGameResolver(),gameResolverValues);
+
+        String[] trainingValues={gameConfig.getDaysUntilStatIncreaseCheck()};
+        config.put(gameConfig.getTraining(),trainingValues);
+
+        String[] tradingValues={gameConfig.getLossPoint(),gameConfig.getRandomTradeOfferChance(),gameConfig.getMaxPlayersPerTrade(),gameConfig.getRandomAcceptanceChance()};
+        config.put(gameConfig.getTrading(),tradingValues);
+
+        for(String key:config.keySet()){
+            String[] array=config.get(key);
+            for(String subCategory : array){
+                insertConfigValues(key, subCategory,gameConfig.getValueFromOurObject(key,subCategory));
+            }
+        }
+
+
+    }
+
+    public void insertConfigValues(String category,String subCategory,String configValue) throws Exception {
         try {
             CallStoredProcedure callproc = new CallStoredProcedure("insertGamePlayConfig(?,?,?,?)");
-//            callproc.setParameter(1, category);
-//            callproc.setParameter(2, subCategory);
-//            callproc.setParameter(3, configValue);
-//            callproc.setParameter(4, leagueName);
+            callproc.setParameter(1, category);
+            callproc.setParameter(2, subCategory);
+            callproc.setParameter(3, configValue);
+            callproc.setParameter(4, leagueName);
             callproc.execute();
             callproc.cleanup();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        }  catch (Exception exception) {
+            throw  new Exception("Error inserting config in DB");
         }
     }
 
@@ -27,25 +59,64 @@ public class gameConfigDB implements IGameConfigDB {
         String configValue = "";
         try {
             CallStoredProcedure callproc = new CallStoredProcedure("loadGamePlayConfig(?)");
-
             callproc.setParameter(1, leagueName);
             ResultSet results = callproc.executeWithResults();
-
+            JSONObject subObj=new JSONObject();
+            JSONObject mainObj = new JSONObject();
+            String category="";
             if (null != results) {
                 while (results.next()) {
-                    configValue = results.getString("configValue");
+                    category=results.getString("category");
+                    subObj.put(results.getString("subCategory"),results.getString("configValue"));
                 }
+                mainObj.put(category,subObj);
+
+                subObj=new JSONObject();
+                ResultSet gameResolverResultSet = callproc.getMoreResults();
+                while (gameResolverResultSet.next()) {
+                    category=gameResolverResultSet.getString("category");
+                    subObj.put(gameResolverResultSet.getString("subCategory"),gameResolverResultSet.getString("configValue"));
+                }
+                mainObj.put(category,subObj);
+
+                subObj=new JSONObject();
+                ResultSet injuryResultSet = callproc.getMoreResults();
+                while (injuryResultSet.next()) {
+                    category=injuryResultSet.getString("category");
+                    subObj.put(injuryResultSet.getString("subCategory"),injuryResultSet.getString("configValue"));
+                }
+                mainObj.put(category,subObj);
+
+                subObj=new JSONObject();
+                ResultSet trainingResultSet = callproc.getMoreResults();
+                while (trainingResultSet.next()) {
+                    category=trainingResultSet.getString("category");
+                    subObj.put(trainingResultSet.getString("subCategory"),trainingResultSet.getString("configValue"));
+                }
+                mainObj.put(category,subObj);
+
+                subObj=new JSONObject();
+                ResultSet tradingResultSet = callproc.getMoreResults();
+                while (tradingResultSet.next()) {
+                    category=tradingResultSet.getString("category");
+                    subObj.put(tradingResultSet.getString("subCategory"),tradingResultSet.getString("configValue"));
+                }
+                mainObj.put(category,subObj);
+
             }
             else {
                 throw new Exception("Game config not loaded properly");
             }
-
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("gameplayConfig",mainObj);
+            gameConfig=new GameConfig(jsonObject);
             callproc.cleanup();
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (Exception e ) {
+            e.printStackTrace();
         }
 
         return gameConfig;
     }
+
+
 }
