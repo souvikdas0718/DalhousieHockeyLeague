a3ddb48bcd524e70f46e358af089aa01ff63ce0a
souvikdas <Souvik.Das@dal.ca>
2020-11-26 18:31:05 -0400

    Adding Creational pattern for GameContext and SeasonSimulation

---
+++ src/main/java/dhl/businessLogic/simulationStateMachine/factory/ContextFactory.java
@@ -0,0 +1,15 @@
+package dhl.businessLogic.simulationStateMachine.factory;
+
+import dhl.businessLogic.simulationStateMachine.GameContext;
+import dhl.businessLogic.simulationStateMachine.SimulationContext;
+
+public class ContextFactory extends ContextAbstractFactory{
+
+    public GameContext createGameContext() {
+        return new GameContext();
+    }
+
+    public SimulationContext createSimulationContext() {
+        return new SimulationContext(createGameContext());
+    }
+}
