8adaf15ebdeec39264294562b67d56256e97cbdb
Rashmi Chandy <rs682521@dal.ca>
2020-11-29 22:48:40 +0530

    Adding tests for player draft(LOM)

--- src/test/java/dhl/businessLogicTest/leagueModelTests/PlayerDraftTest.java
+++ src/test/java/dhl/businessLogicTest/leagueModelTests/PlayerDraftTest.java
@@ -1,64 +1,54 @@
 package dhl.businessLogicTest.leagueModelTests;
 
-import dhl.businessLogic.leagueModel.PlayerDraft;
+import dhl.businessLogic.leagueModel.Team;
 import dhl.businessLogic.leagueModel.factory.LeagueModelAbstractFactory;
 import dhl.businessLogic.leagueModel.interfaceModel.*;
 import dhl.businessLogicTest.leagueModelTests.factory.LeagueModelMockAbstractFactory;
+import dhl.businessLogicTest.leagueModelTests.mocks.TeamMock;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
-
 public class PlayerDraftTest {
-/*
-    IPlayerDraft testClassObject;
-    ILeagueObjectModel league;
+    IPlayerDraft playerDraft;
     LeagueModelAbstractFactory leagueFactory;
     LeagueModelMockAbstractFactory leagueMockFactory;
     ITeam[][] draftPickSequenceMock;
+    ITeam team1;
+    ITeam team2;
+
     @BeforeEach
     public void initObject(){
         leagueFactory = LeagueModelAbstractFactory.instance();
         leagueMockFactory = LeagueModelMockAbstractFactory.instance();
-        league = leagueMockFactory.createLeagueMock().getLeagueObjectModel();
-        initDraft(league);
 
-        testClassObject = leagueFactory.createPlayerDraft();
+        playerDraft = leagueFactory.createPlayerDraft();
 
-    }
+        TeamMock teamMock = leagueMockFactory.createTeamMock();
+        team1 = teamMock.getTeamByName("Ontario");
+        team2 = teamMock.getTeamByName("Torronto");
+        draftPickSequenceMock = new Team [2][2];
+        draftPickSequenceMock[0][0] = team1;
+        draftPickSequenceMock[0][1] = team2;
+        draftPickSequenceMock[1][0] = team1;
+        draftPickSequenceMock[1][1] = team2;
 
-    // TODO: 28-11-2020 mock for this
-    public void initDraft(ILeagueObjectModel league){
-        int totalRound = 7;
-        ArrayList<ITeam> list = new ArrayList<>();
-        for(IConference c: league.getConferences()){
-            for (IDivision d: c.getDivisions()){
-                for (ITeam t: d.getTeams()){
-                    list.add(t);
-                }
-            }
-        }
-        draftPickSequenceMock = new ITeam[list.size()][totalRound];
-        for (int i = 0; i < list.size(); i++){
-            for (int j = 0; j < totalRound; j++ ){
-                draftPickSequenceMock[i][j] = list.get(i);
-            }
-        }
     }
 
     @Test
     public void swapDraftPickTest(){
-
-        ITeam teamGivingDraftPick = draftPickSequenceMock[0][1];
-        ITeam teamGettingDraftPick = draftPickSequenceMock[1][1];
-
-        testClassObject.swapDraftPick(1,teamGettingDraftPick,teamGivingDraftPick);
-        draftPickSequenceMock = testClassObject.getDraftPickSequence();
+        playerDraft.setDraftPickSequence(draftPickSequenceMock);
+        ITeam teamGettingDraft = team1;
+        ITeam teamGivingDraft = team2;
+        playerDraft.swapDraftPick(1, teamGettingDraft, teamGivingDraft);
         ITeam testTeam = draftPickSequenceMock[1][0];
-        Assertions.assertEquals(testTeam,teamGettingDraftPick);
+        Assertions.assertEquals(testTeam,teamGettingDraft);
     }
 
-*/
+    @Test
+    public void setDraftPickSequenceTest() {
+        playerDraft.setDraftPickSequence(draftPickSequenceMock);
+        Assertions.assertTrue(playerDraft.getDraftPickSequence().length>0);
+    }
+
 }
