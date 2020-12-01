b780f62022cd7344600e89aa896662a1b179001a
Rashmi Chandy <rs682521@dal.ca>
2020-12-01 06:02:35 +0530

    Player draft state changes

--- src/main/java/dhl/businessLogic/simulationStateMachine/states/seasonSimulation/InitializeSeasonState.java
+++ src/main/java/dhl/businessLogic/simulationStateMachine/states/seasonSimulation/InitializeSeasonState.java
@@ -1,6 +1,10 @@
 package dhl.businessLogic.simulationStateMachine.states.seasonSimulation;
 
 
+import dhl.businessLogic.leagueModel.PlayerDraftAbstract;
+import dhl.businessLogic.leagueModel.Team;
+import dhl.businessLogic.leagueModel.factory.LeagueModelAbstractFactory;
+import dhl.businessLogic.leagueModel.interfaceModel.*;
 import dhl.businessLogic.simulationStateMachine.SimulationContext;
 import dhl.businessLogic.simulationStateMachine.states.seasonScheduler.factory.SchedulerAbstractFactory;
 import dhl.businessLogic.simulationStateMachine.states.seasonScheduler.interfaces.IScheduler;
@@ -13,11 +17,15 @@ import org.apache.logging.log4j.Logger;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.time.temporal.TemporalAdjusters.firstDayOfNextMonth;
 import static java.time.temporal.TemporalAdjusters.nextOrSame;
 
 public class InitializeSeasonState implements ISimulationSeasonState {
+    private static final int DRAFTROUNDS = 7;
+    private static final int NOOFTEAMS = 32;
     private static final int SIMULATESTARTMONTH = 9;
     private static final int SIMULATESTARTDAY = 30;
     private static final int REGULARSEASONENDMONTH = 3;
@@ -25,12 +33,16 @@ public class InitializeSeasonState implements ISimulationSeasonState {
     private static final int DAY = 1;
     private static final int YEAR = 1;
     public static Logger logger = LogManager.getLogger(InitializeSeasonState.class);
+    ITeam[][] draftPickSequence = new Team[NOOFTEAMS][DRAFTROUNDS];
     SimulationContext simulationContext;
     IScheduler scheduler;
     SchedulerAbstractFactory schedulerAbstractFactory;
     StandingsAbstractFactory standingsAbstractFactory;
     IUserInputOutput userInputOutput;
     IStandingSystem standingSystem;
+    ILeagueObjectModel leagueObjectModel;
+    LeagueModelAbstractFactory leagueFactory;
+    PlayerDraftAbstract playerDraft;
 
     public InitializeSeasonState(SimulationContext simulationContext) {
         logger.info("Into the Initialize season constructor");
@@ -54,6 +66,10 @@ public class InitializeSeasonState implements ISimulationSeasonState {
     public void seasonStateProcess() {
         userInputOutput.printMessage("Into the state process of Initialize season");
         logger.info("Into the state process of Initialize season");
+        initializePlayerDraftPick();
+        leagueFactory = LeagueModelAbstractFactory.instance();
+        playerDraft = leagueFactory.createPlayerDraft();
+        playerDraft.setDraftPickSequence(draftPickSequence);
         LocalDate simulationStartDate = LocalDate.of(simulationContext.getYear(), SIMULATESTARTMONTH, SIMULATESTARTDAY);
         simulationContext.setStartOfSimulation(simulationStartDate);
         scheduler.generateTeamList(simulationContext.getInMemoryLeague());
@@ -72,4 +88,41 @@ public class InitializeSeasonState implements ISimulationSeasonState {
         logger.info("Into the exit process of Initialize season");
         simulationContext.setCurrentSimulation(simulationContext.getAdvanceTime());
     }
+
+    public void initializePlayerDraftPick(){
+        logger.info("Initialize player draft pick");
+        List<ITeam> teamsInLeague = getTeams();
+        if(teamsInLeague.size()>0){
+            for (int i = 0; i < NOOFTEAMS; i++) {
+                for (int j = 0; j < DRAFTROUNDS; j++) {
+                    logger.debug("Initialize player draft pick for team");
+                    draftPickSequence[i][j]=teamsInLeague.get(i);
+                }
+            }
+        }
+
+    }
+
+    public List<ITeam>  getTeams(){
+        List<ITeam> teamsInLeague  = new ArrayList<>();
+        logger.info("Fetching all teams in league");
+        logger.debug("Fetching teams in league to initialize player draft pick 2D array");
+        leagueObjectModel = simulationContext.getInMemoryLeague();
+        for(IConference conference:leagueObjectModel.getConferences()){
+            for(IDivision division:conference.getDivisions()){
+                for(ITeam team:division.getTeams()){
+                    if(checkIfUserTeam(team.getTeamName())){
+                        teamsInLeague.add(team);
+                    }
+                }
+            }
+        }
+        return teamsInLeague;
+    }
+
+    public boolean checkIfUserTeam(String teamName){
+        ITeam userTeam = this.simulationContext.getUserTeam();
+        return !teamName.equals(userTeam.getTeamName());
+    }
+
 }
